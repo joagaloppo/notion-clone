d6109dfa7a80aa3b603d03e779c05b49688740b8
Joaquin Galoppo <joaquingaloppo@gmail.com>
2023-11-08 09:39:16 -0300

    Fix search error

--- components/search-command.tsx
+++ components/search-command.tsx
@@ -62,9 +62,9 @@ const SearchCommand: React.FC<SearchCommandProps> = () => {
           {documents?.map((document) => (
             <CommandItem
               key={document._id}
-              value={`${document._id}`}
+              value={`${document._id}-${document.title}`}
               title={document.title}
-              onSelect={onSelect}
+              onSelect={() => onSelect(document._id)}
             >
               {document.icon ? (
                 <p className="mr-2 text-[18px]">{document.icon}</p>
